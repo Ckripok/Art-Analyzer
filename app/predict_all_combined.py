43886511b6799d93e131d34af5bf7925155321fc
Pavlov <117574097+Ckripok@users.noreply.github.com>
2025-06-08 13:38:55 +0800

    ix: enable use_safetensors for BLIP model

--- app/predict_all_combined.py
+++ app/predict_all_combined.py
@@ -62,8 +62,10 @@ except FileNotFoundError:
     style_classes = []
 
 blip_processor = BlipProcessor.from_pretrained("Salesforce/blip-image-captioning-base")
-blip_model = BlipForConditionalGeneration.from_pretrained("Salesforce/blip-image-captioning-base", trust_remote_code=True, use_safetensors=True)
-
+blip_model = BlipForConditionalGeneration.from_pretrained(
+    "Salesforce/blip-image-captioning-base",
+    use_safetensors=True
+)
 
 transform = transforms.Compose([
     transforms.Resize((IMG_SIZE, IMG_SIZE)),
