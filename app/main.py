98c9d1df444ca03ceb44b3300f87fcad8d22c327
Pavlov <117574097+Ckripok@users.noreply.github.com>
2025-06-08 12:12:53 +0800

    /

--- app/main.py
+++ app/main.py
@@ -20,29 +20,29 @@ static_genre_path = os.path.join(BASE_DIR, "dataset_genre", "train")
 if os.path.exists(static_genre_path):
     app.mount("/static_examples_genre", StaticFiles(directory=static_genre_path), name="static_examples_genre")
 
-
 @app.get("/")
 async def home():
-    return FileResponse(os.path.join(BASE_DIR, "templates", "main.html"))
+    return FileResponse(os.path.join(BASE_DIR, "..", "templates", "main.html"))
+
 
 @app.get("/main.html")
 async def main_page():
-    return FileResponse(os.path.join(BASE_DIR, "templates", "main.html"))
+    return FileResponse(os.path.join(BASE_DIR, "..", "templates", "main.html"))
 
 
 @app.get("/genres.html")
 async def genres_page():
-    return FileResponse(os.path.join(BASE_DIR, "templates", "genres.html"))
+    return FileResponse(os.path.join(BASE_DIR, "..", "templates", "genres.html"))
 
 
 @app.get("/styles.html")
 async def styles_page():
-    return FileResponse(os.path.join(BASE_DIR, "templates", "styles.html"))
+    return FileResponse(os.path.join(BASE_DIR, "..", "templates", "styles.html"))
 
 
 @app.get("/api/genres")
 async def get_genres():
-    base_path = os.path.join(BASE_DIR, "dataset_genre", "train")
+    base_path = os.path.join(BASE_DIR, "..", "dataset_genre", "train")
 
     genres = []
     for folder in sorted(os.listdir(base_path)):
@@ -63,7 +63,7 @@ async def get_genres():
 
 @app.get("/api/styles")
 async def get_styles():
-    base_path = os.path.join(BASE_DIR, "dataset_styles", "train")
+    base_path = os.path.join(BASE_DIR, "..", "dataset_styles", "train")
     styles = []
     for folder in sorted(os.listdir(base_path)):
         folder_path = os.path.join(base_path, folder)
@@ -85,17 +85,17 @@ async def get_styles():
 
 @app.get("/analysis.html")
 async def analysis_page():
-    return FileResponse(os.path.join(BASE_DIR, "templates", "analysis.html"))
+    return FileResponse(os.path.join(BASE_DIR, "..", "templates", "analysis.html"))
 
 
 @app.get("/about.html")
 async def about():
-    return FileResponse(os.path.join(BASE_DIR, "templates", "about.html"))
+    return FileResponse(os.path.join(BASE_DIR, "..", "templates", "about.html"))
 
 
 @app.get("/contacts.html")
 async def contacts():
-    return FileResponse(os.path.join(BASE_DIR, "templates", "contacts.html"))
+    return FileResponse(os.path.join(BASE_DIR, "..", "templates", "contacts.html"))
 
 
 @app.post("/predict_all_combined")
@@ -105,7 +105,7 @@ async def predict(file: UploadFile = File(...)):
 
     # Сохраняем CAM-карту в static/
     cam_filename = f"{uuid.uuid4().hex}.jpg"
-    cam_path = os.path.join(BASE_DIR, "static", cam_filename)
+    cam_path = os.path.join(BASE_DIR, "..", "static", cam_filename)
 
     results = predict_image_top3(image, save_cam_path=cam_path)
 
